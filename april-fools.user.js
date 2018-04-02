6d3d1148f588c39a7feaa5127a541b15030555c3
7twin <32747235+7twin@users.noreply.github.com>
2018-04-02 17:36:50 +0200

    added .io and profile score matching

--- april-fools.user.js
+++ april-fools.user.js
@@ -1,18 +1,25 @@
 // ==UserScript==
 // @name         Devrant April Fools 2018
 // @namespace    https://devrant.com/
-// @version      0.1
+// @version      0.3
 // @description  convert all scores to binary
 // @author       7twin
+
 // @match        https://devrant.com/feed/*
 // @match        https://devrant.com/rants/*
+// @match        https://devrant.com/users/*
+
+// @match        https://devrant.io/feed/*
+// @match        https://devrant.io/rants/*
+// @match        https://devrant.io/users/*
+
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    $(".user-score span").each(function(){
+    $(".user-score span,.profile-score").each(function(){
         $(this).html(($(this).html() >>> 0).toString(2));
     });
 })();
