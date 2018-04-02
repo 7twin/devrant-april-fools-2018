bdb40ab676a6145484d639773ea9c0da17aa429e
7twin <32747235+7twin@users.noreply.github.com>
2018-04-02 05:18:25 +0200

    Create april-fools.user.js

---
+++ april-fools.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Devrant April Fools 2018
+// @namespace    https://devrant.com/
+// @version      0.1
+// @description  convert all scores to binary
+// @author       7twin
+// @match        https://devrant.com/rants/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $(".user-score span").each(function(){
+        $(this).html(($(this).html() >>> 0).toString(2));
+    });
+})();
